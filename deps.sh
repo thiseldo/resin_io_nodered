f5960baac815c08468662dc9653d28ef94daa998
thinginnovations <andrew@thinginnovations.com>
2014-11-11 20:08:46 +0000

    first

---
+++ deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+
+apt-get -q update
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+cd ..
+git clone https://github.com/node-red/node-red.git
+cd node-red
+npm install wiring-pi
+npm install js2xmlparser
+npm install serialport
+npm install feedparser 
+npm install --production
+
+cp -f ../settings.js .
+cp -f ../flows_raspberrypi.json .
