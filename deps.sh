b5fffaa076b731ce75cb45c0ab90255549346f94
Andrew Lindsay <andrew@thiseldo.co.uk>
2014-12-02 16:35:01 +0000

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -9,10 +9,13 @@ cd wiringPi
 cd ..
 git clone https://github.com/node-red/node-red.git
 cd node-red
+cd nodes
+git clone https://github.com/node-red/node-red-nodes.git
+cd ..
 npm install wiring-pi
 npm install js2xmlparser
 npm install serialport
-npm install feedparser 
+npm install feedparser
 npm install --production
 
 cp -f ../settings.js .
