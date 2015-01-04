b1ccbb9b5066fa478572945396ab9edb8d61cf80
thinginnovations <andrew@thinginnovations.com>
2015-01-04 14:35:05 +0000

    updates for latest node-red and RPi GPIO

--- deps.sh
+++ deps.sh
@@ -3,8 +3,11 @@
 set -o errexit
 
 apt-get -q update
-apt-get -q upgrade
-apt-get -q install python-dev python-rpi.gpio
+apt-get -q -y upgrade
+apt-get -q -y install python build-essential python-dev python-pip
+# Install python modules
+#pip install RPi.GPIO
+dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
 
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
